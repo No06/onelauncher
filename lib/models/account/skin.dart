8258df8d369033ff18c9061f62d6f42bc88f6dc8
Noo6 <2746419228@qq.com>
2024-04-14 22:15:11 +0800

    使用异步处理皮肤 Skin 图像，可能提高了性能

--- lib/models/account/skin.dart
+++ lib/models/account/skin.dart
@@ -18,19 +18,24 @@ abstract class Skin {
     final wratio = source!.width ~/ 64;
     final lratio =
         (source.height == source.width) ? wratio : source.height ~/ 32;
-    final face = copyResize(
-        copyCrop(source, x: 8 * wratio, y: 8 * lratio, width: 8, height: 8),
-        width: 64,
-        height: 64);
-    final hair = copyResize(
-        copyCrop(source, x: 40 * wratio, y: 8 * lratio, width: 8, height: 8),
-        width: 72,
-        height: 72);
+    late final Image face;
+    late final Image hair;
+    await Future.wait([
+      Future(() => face = copyResize(
+          copyCrop(source, x: 8 * wratio, y: 8 * lratio, width: 8, height: 8),
+          width: 64,
+          height: 64)),
+      Future(() => hair = copyResize(
+          copyCrop(source, x: 40 * wratio, y: 8 * lratio, width: 8, height: 8),
+          width: 72,
+          height: 72)),
+    ]);
     final head = Image(width: 72, height: 72, numChannels: 4);
-    head.clear(ColorInt8.rgba(0, 0, 0, 0));
-    compositeImage(head, face, center: true);
-    compositeImage(head, hair, center: true);
-    return encodePng(head);
+    await Future.wait([
+      Future(() => compositeImage(head, face, center: true)),
+      Future(() => compositeImage(head, hair, center: true)),
+    ]);
+    return encodePng(head, singleFrame: true, filter: PngFilter.none);
   }
 
   JsonMap toJson() => _$SkinToJson(this);
