68630971580441b5b43f8371813085b5941cf288
Noo6 <2746419228@qq.com>
2024-05-22 20:06:12 +0800

    优化代码结构

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,14 +1,13 @@
-import 'package:one_launcher/main.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 const appName = "OneLauncher";
 const kConfigName = "config.json";
-final kGameConfigName = "${appInfo.appName}.json";
-final kConfigDirectoryName = appInfo.appName;
-final kConfigPath = Future(() async => join(
-    (await getApplicationDocumentsDirectory()).path, kConfigDirectoryName));
+const kGameConfigName = "$appName.json";
+const kConfigDirectoryName = appName;
+final kConfigPath =
+    Future(() async => join((await getApplicationDocumentsDirectory()).path, kConfigDirectoryName));
 
 const kDefaultBorderRadius = BorderRadius.all(Radius.circular(7.5));
 const kMediaBorderRadius = BorderRadius.all(Radius.circular(10));
--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,10 @@ import 'package:get_storage/get_storage.dart';
 import 'package:one_launcher/models/config/app_config.dart';
 import 'package:one_launcher/utils/java_util.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
-
-late final PackageInfo appInfo;
 
 void main() async {
   await init();
@@ -19,37 +16,31 @@ void main() async {
 
 Future<void> init() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // debug 模式下擦除持久化数据
-  // if (kDebugMode) {
-  //   await GetStorage().erase();
-  // }
+
+  initWindow() async {
+    await windowManager.ensureInitialized();
+    const defaultWindowWidth = 960.0;
+    const defaultWindowHeight = 593.0;
+    const windowSize = Size(defaultWindowWidth, defaultWindowHeight);
+    final windowOptions = WindowOptions(
+      size: windowSize,
+      minimumSize: windowSize,
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle:
+          Platform.isWindows ? TitleBarStyle.hidden : TitleBarStyle.normal,
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.setAsFrameless();
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
   await Future.wait([
-    PackageInfo.fromPlatform().then((value) => appInfo = value),
     JavaUtil.init(),
-    windowManager.ensureInitialized(),
+    initWindow(),
+    GetStorage.init().then((value) => AppConfig.init()),
   ]);
-  // 获取持久化配置文件
-  await GetStorage.init().then(
-    (value) => AppConfig.init(),
-  );
-  // 初始化窗口
-  const defaultWindowWidth = 960.0;
-  const defaultWindowHeight = 593.0;
-  const windowSize = Size(defaultWindowWidth, defaultWindowHeight);
-  final windowOptions = WindowOptions(
-    size: windowSize,
-    minimumSize: windowSize,
-    center: true,
-    backgroundColor: Colors.transparent,
-    skipTaskbar: false,
-    titleBarStyle:
-        Platform.isWindows ? TitleBarStyle.hidden : TitleBarStyle.normal,
-  );
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await Future.wait([
-      windowManager.setAsFrameless(),
-      windowManager.show(),
-      windowManager.focus(),
-    ]);
-  });
 }
