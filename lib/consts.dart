2724690ca148b4491a7ea6c9813ebba5b1028e8c
Noo6 <2746419228@qq.com>
2024-07-11 19:33:12 +0800

    添加 kAzureClientId 常量

--- lib/consts.dart
+++ lib/consts.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 const appName = "OneLauncher";
 final kGameDirectoryName = Platform.isWindows ? ".minecraft" : "minecraft";
 final kHideTitleBar = Platform.isWindows;
+const kAzureClientId = "8b6fb1f0-7e3e-41d3-8171-53ff17134e00";
 
 const kDefaultBorderRadius = BorderRadius.all(Radius.circular(7.5));
 const kMediaBorderRadius = BorderRadius.all(Radius.circular(10));
